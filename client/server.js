219552eeaaf18a51e6f4d9a246e51ba8b90eaa30
VasVV <radioheadboy0@gmail.com>
2021-07-05 12:42:30 +0200

    fixes

--- client/server.js
+++ client/server.js
@@ -15,12 +15,7 @@ app.use(
   
   app.use(express.json())
 
-  if (process.env.NODE_ENV === "production") {
-    app.use(express.static("build"));
-    app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname,  "build", "index.html"));
-    });
-  }
+ 
 
 
 app.post('/sendmail', async (req, res) => {
